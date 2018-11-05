4ddae13fd34de28b031ad20669645bf51b7cd87c
amengngema <vladimircampos81@gmail.com>
2018-11-05 14:40:57 +0800

    add changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { PostsComponent } from './posts/posts.component';
+import { UsersComponent } from './users/users.component';
+import { DeatailsComponent } from './deatails/deatails.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    PostsComponent,
+    UsersComponent,
+    DeatailsComponent
   ],
   imports: [
     BrowserModule,
